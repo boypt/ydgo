217d35baa4893e4933e72aa66eae5db767768ee1
boypt <pentie@gmail.com>
2019-03-08 15:55:51 +0800

    add go.mod

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/boypt/ydgo
+
+require (
+	github.com/antonholmquist/jason v1.0.0
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.1 // indirect
+	github.com/mattn/go-isatty v0.0.6 // indirect
+)
--- make_release.sh
+++ make_release.sh
@@ -6,18 +6,9 @@ GITVER=$(git rev-parse --short HEAD)
 
 rm -fv ydgo_*
 
-if [[ -e /proc/sys/kernel/osrelease ]] &&  grep -q -i microsoft /proc/sys/kernel/osrelease && command -v go.exe; then
-  # for WSL 
-  for OS in darwin linux; do
-    env WSLENV=CGO_ENABLE:GOARCH:GOOS CGO_ENABLED=0 GOARCH=amd64 GOOS=$OS cmd.exe /C go.exe build -o ${BIN}_${OS} -ldflags "-s -w -X main.VERSION=$GITVER"
-  done
-  env WSLENV=CGO_ENABLE:GOARCH:GOOS CGO_ENABLED=0 GOARCH=amd64 GOOS=windows cmd.exe /C go.exe build -o ${BIN}_windows.exe -ldflags "-s -w -X main.VERSION=$GITVER"
-else
-
-  # for normal unix env
-  for OS in darwin linux; do
-    CGO_ENABLED=0 GOARCH=amd64 GOOS=$OS go build -o ${BIN}_${OS} -ldflags "-s -w -X main.VERSION=$GITVER"
-  done
-  CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -o ${BIN}_windows.exe -ldflags "-s -w -X main.VERSION=$GITVER"
-fi	
+# for normal unix env
+for OS in darwin linux; do
+  CGO_ENABLED=0 GOARCH=amd64 GOOS=$OS go build -o ${BIN}_${OS} -ldflags "-s -w -X main.VERSION=$GITVER"
+done
+CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -o ${BIN}_windows.exe -ldflags "-s -w -X main.VERSION=$GITVER"
 
