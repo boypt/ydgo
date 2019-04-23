e48d6eff418ac00486e05f5e5c0148211142daf7
boypt <pentie@gmail.com>
2019-04-23 13:33:21 +0800

    use VER

--- make_release.sh
+++ make_release.sh
@@ -13,7 +13,7 @@ rm -fv ydgo_*
 
 # for normal unix env
 for OS in darwin linux; do
-  CGO_ENABLED=0 GOARCH=amd64 GOOS=$OS go build -o ${BIN}_${OS} -ldflags "-s -w -X main.VERSION=$GITVER"
+  CGO_ENABLED=0 GOARCH=amd64 GOOS=$OS go build -o ${BIN}_${OS} -ldflags "-s -w -X main.VERSION=$VER"
 done
-CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -o ${BIN}_windows.exe -ldflags "-s -w -X main.VERSION=$GITVER"
+CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -o ${BIN}_windows.exe -ldflags "-s -w -X main.VERSION=$VER"
 
