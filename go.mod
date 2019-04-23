e7b5092fe33df2cde8ecfbeb11b836a36bf5f2c1
boypt <pentie@gmail.com>
2019-04-23 10:45:56 +0800

    appkey read from ~/.ydgo

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/boypt/ydgo
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/antonholmquist/jason v1.0.0
 	github.com/fatih/color v1.7.0
 	github.com/mattn/go-colorable v0.1.1 // indirect
--- main.go
+++ main.go
@@ -12,17 +12,20 @@ import (
 	"os"
 	"strings"
 
+	"github.com/BurntSushi/toml"
 	"github.com/antonholmquist/jason"
 	"github.com/fatih/color"
 )
 
 var VERSION string = "0.0-src" //set with ldflags
-const (
-	YDAPPKEY = "1d9b4cc7c9694745"
-	YDSECKEY = "U9IEK5Qc4CMuWGvbsrwBXaeO6KO7xZwJ"
-)
+
+type Config struct {
+	YDAppKey string
+	YDSecKey string
+}
 
 var debug bool = false
+var config Config
 
 func httpGet(url string) *jason.Object {
 	resp, err := http.Get(url)
@@ -47,6 +50,12 @@ func httpGet(url string) *jason.Object {
 }
 
 func printExplain(q string, v *jason.Object) {
+
+	errorCode, _ := v.GetString("errorCode")
+	if errorCode != "0" {
+		fmt.Fprintf(color.Output, color.HiRedString("ErrorCode: %s\nRefer to: http://ai.youdao.com/docs/doc-trans-api.s#p08\n", errorCode))
+		return
+	}
 
 	query, err := v.GetString("query")
 	if err != nil {
@@ -108,14 +117,14 @@ func ydAPI(query string, from string) string {
 	salt := rand.Int31()
 
 	// assume query is in utf-8
-	signstr := fmt.Sprintf("%s%s%d%s", YDAPPKEY, query, salt, YDSECKEY)
+	signstr := fmt.Sprintf("%s%s%d%s", config.YDAppKey, query, salt, config.YDSecKey)
 	sign := md5.Sum([]byte(signstr))
 	uri := fmt.Sprintf(
 		"https://openapi.youdao.com/api?appKey=%s&q=%s&from=%s&to=zh-CHS&salt=%d&sign=%x",
-		YDAPPKEY, url.QueryEscape(query), from, salt, sign)
+		config.YDAppKey, url.QueryEscape(query), from, salt, sign)
 
 	if debug {
-		log.Println(uri)
+		log.Printf("Req: %s", uri)
 	}
 	return uri
 }
@@ -153,29 +162,29 @@ func showHelp() {
 }
 
 func main() {
-	var interative, help bool
-	var from string
+	var interative bool
+	var configfile, from string
 	flag.BoolVar(&interative, "i", false, "interative mode, :q  \\q  EOF or Ctrl+C to exit.")
-	flag.BoolVar(&help, "h", false, "show this help")
 	flag.BoolVar(&debug, "d", false, "log api request")
 	flag.StringVar(&from, "f", "EN", "translate-from language, default: EN")
+	flag.StringVar(&configfile, "c", "~/.ydgo", "translate-from language, default: EN")
 	flag.Parse()
 
-	if help {
-		showHelp()
-		return
+	configfile = NormalizePath(configfile)
+	_, err := os.Stat(configfile)
+	if err != nil {
+		log.Fatal("Config file is missing: ", configfile)
 	}
 
-	if interative {
+	if _, err := toml.DecodeFile(configfile, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	if interative || flag.NArg() == 0 {
 		interativeMode(from)
 		return
 	}
 
-	if len(os.Args) < 2 {
-		showHelp()
-		return
-	}
-
-	query := strings.Join(os.Args[1:], " ")
+	query := strings.Join(flag.Args(), " ")
 	printExplain(query, httpGet(ydAPI(query, from)))
 }
